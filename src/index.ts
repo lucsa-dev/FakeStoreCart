672a556ca8f559ee16ee00c622f25f3a4f47366d
Lucas Almeida <lucsa.dev@gmail.com>
2025-04-24 13:20:27 -0300

    fix: serve swagger files from CDN

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,8 @@ const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 app.use('/add-three-products-to-cart', appController);
 // Servir interface e assets do Swagger em '/docs'
-app.use('/docs', swaggerUi.serve);
-app.get('/docs', swaggerUi.setup(swaggerSpec));
+app.use('/', swaggerUi.serve);
+app.get('/', swaggerUi.setup(swaggerSpec));
 
 
 export default app;
