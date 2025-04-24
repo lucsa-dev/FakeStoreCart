eeb03e63e733ba2698d8baa59287f13e2b612cc0
Lucas Almeida <lucsa.dev@gmail.com>
2025-04-24 09:25:44 -0300

    feat: configure swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,54 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { getProducts } from './modules/product/product.service';
 import { addToCart } from './modules/cart/cart.service';
 
 const router = Router();
 
-router.get('/', async (_req, res) => {
+/**
+ * @swagger
+ * tags:
+ *   name: Cart
+ *   description: Endpoints para operações no carrinho
+ */
+/**
+ * @swagger
+ * /add-three-products-to-cart:
+ *   post:
+ *     summary: Adiciona três produtos ao carrinho
+ *     tags: [Cart]
+ *     responses:
+ *       200:
+ *         description: Itens adicionados ao carrinho com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   example: 11
+ *                 userId:
+ *                   type: integer
+ *                   example: 1
+ *                 products:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       title:
+ *                         type: string
+ *                         example: "Example Product Title"
+ *                       price:
+ *                         type: number
+ *                         example: 10.99
+ *       500:
+ *         description: Serviço indisponível
+ */
+
+router.post('/', async (req: Request, res: Response) => {
   try {
     const products = await getProducts(3);
     const cartItem = {
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 import express from 'express';
 import appController from './app.controller';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 
 app.use(express.json());
 
+// Configuração do Swagger
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'FakeStoreCart API',
+      version: '1.0.0',
+      description: 'Documentação da API FakeStoreCart',
+    },
+    servers: [{ url: 'http://localhost:3000' }],
+  },
+  apis: ['./src/**/*.ts'],
+};
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
 app.use('/add-three-products-to-cart', appController);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default app;
 
